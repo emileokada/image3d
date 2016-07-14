fb6933ef4d0bebfa423275d92fb04e8f18fc2856
Emile Okada <emileokada@gmail.com>
2016-07-14 11:02:49 +0100

    Can now compute convex hull from shadow function

--- shadow_function.py
+++ shadow_function.py
@@ -1,6 +1,16 @@
+from __future__ import division
 from PIL import Image
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import math
 import re
 import os
+
+def differentiate(y, step_size):
+    y_temp = y[-1:] + y + y[:1]
+    return [(t - s)/(2*step_size) for s, t in zip(y_temp, y_temp[2:])]
 
 class image3d:
     def __init__(self,files):
@@ -14,7 +24,7 @@ class image3d:
             img_data = self.img_to_array(img)
             for i, row in enumerate(img_data):
                 (first, last) = self.read_value(row)
-                self.shadow_list[i].insert(len(self.shadow_list[i])/2,first)
+                self.shadow_list[i].insert(len(self.shadow_list[i])//2,first)
                 self.shadow_list[i].append(last)
 
     def preprocess(self,img):
@@ -28,7 +38,7 @@ class image3d:
         return [l[w*i:w*(i+1)] for i in range(h)]
 
     def read_value(self,img_row):
-        mid_point = len(img_row)/2
+        mid_point = len(img_row)//2
         try:
             first = mid_point - img_row.index(0)
         except ValueError:
@@ -38,10 +48,16 @@ class image3d:
         except ValueError:
             last = 0
         return (first, last)
+    def convex_hull(self,shadow_value):
+        theta = np.linspace(0,2*math.pi,2*self.resolution)
+        shadow_derivative = differentiate(shadow_value, math.pi/self.resolution)
+        return [(h*math.cos(t)-s*math.sin(t),b*math.sin(t)+s*cos(t)) for t, h, s in zip(theta,shadow_value,shadow_derivate)]
 
 files = ['./Pictures/'+f for f in os.listdir('./Pictures/')]
 files = sorted(files, key=lambda x: int(re.search(r'(\d+)\.jpg',x).group(1)))
 
 space_shuttle = image3d(files)
-
-
+x = np.linspace(0,2*math.pi,2*80)
+plt.plot(x,space_shuttle.shadow_list[100],'-',linewidth=2)
+plt.plot(x,differentiate(space_shuttle.shadow_list[100],math.pi/80),'-',linewidth=1)
+plt.show()
