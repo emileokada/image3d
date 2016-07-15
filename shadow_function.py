e65c3caeb5a083c75e0d14f3335423a400fc340e
Emile Okada <emileokada@gmail.com>
2016-07-15 09:51:39 +0100

    Finished 2D convex hull function

--- shadow_function.py
+++ shadow_function.py
@@ -8,9 +8,30 @@ import math
 import re
 import os
 
+def binomial(n,k):
+    return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
+
+def binomial_density(n):
+    return [binomial(n,k)/(2**n) for k in range(n+1)]
+
+def dot_product(lista,listb):
+    return sum(a*b for a,b in zip(lista,listb))
+
 def differentiate(y, step_size):
     y_temp = y[-1:] + y + y[:1]
     return [(t - s)/(2*step_size) for s, t in zip(y_temp, y_temp[2:])]
+
+def list_convolve(l,kernel):
+    left = (len(kernel)-1)//2
+    right = len(kernel)//2
+    l_temp = l
+    if left > 0:
+        l_temp = l[-left:] + l + l[:right]
+    elif right > 0:
+        l_temp = l + l[:right]
+    if kernel != []:
+        return [dot_product(l_temp[i:i+len(kernel)],kernel) for i in range(len(l_temp)-len(kernel)+1)]
+    return l
 
 class image3d:
     def __init__(self,files):
@@ -26,6 +47,9 @@ class image3d:
                 (first, last) = self.read_value(row)
                 self.shadow_list[i].insert(len(self.shadow_list[i])//2,first)
                 self.shadow_list[i].append(last)
+
+        for i in range(len(self.shadow_list)):
+            self.shadow_list[i] = list_convolve(self.shadow_list[i],binomial_density(10))
 
     def preprocess(self,img):
         img = img.convert('L')
@@ -48,16 +72,16 @@ class image3d:
         except ValueError:
             last = 0
         return (first, last)
+
     def convex_hull(self,shadow_value):
         theta = np.linspace(0,2*math.pi,2*self.resolution)
         shadow_derivative = differentiate(shadow_value, math.pi/self.resolution)
-        return [(h*math.cos(t)-s*math.sin(t),b*math.sin(t)+s*cos(t)) for t, h, s in zip(theta,shadow_value,shadow_derivate)]
+
+        return zip(*[(h*math.cos(t)-s*math.sin(t),h*math.sin(t)+s*math.cos(t)) for t, h, s in zip(theta,shadow_value,shadow_derivative)])
 
 files = ['./Pictures/'+f for f in os.listdir('./Pictures/')]
 files = sorted(files, key=lambda x: int(re.search(r'(\d+)\.jpg',x).group(1)))
 
 space_shuttle = image3d(files)
-x = np.linspace(0,2*math.pi,2*80)
-plt.plot(x,space_shuttle.shadow_list[100],'-',linewidth=2)
-plt.plot(x,differentiate(space_shuttle.shadow_list[100],math.pi/80),'-',linewidth=1)
+plt.plot(*space_shuttle.convex_hull(space_shuttle.shadow_list[290]))
 plt.show()
